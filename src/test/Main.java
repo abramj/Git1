665a3886dfc0ab1d8ecb1d3b5fb759e8f6eae55a
abramj <abramjujunashvili@yahoo.com>
2013-06-29 17:18:54 -0600

    Update Main.java

--- src/test/Main.java
+++ src/test/Main.java
@@ -2,11 +2,13 @@ package test;
 
 public class Main {
 	
+	//Server: Line 1
 	//Local3: added again
 	//comment
 	//Local: this is the conflicting text
 	//Another conflicting text fromserver
 	//Local: Another conflicting text from local
 	//Local2: added text
+	//Server: Last line
 
 }
