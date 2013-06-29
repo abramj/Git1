a7bf9a74b4be1a6590e68243a0aaab87920bf7cb
abramj <abramjujunashvili@yahoo.com>
2013-06-29 17:21:44 -0600

    self merged

--- src/test/Main.java
+++ src/test/Main.java
@@ -4,10 +4,12 @@ public class Main {
 	
 	//Server: Line 1
 	//Local3: added again
+	//Local: second line
 	//comment
 	//Local: this is the conflicting text
 	//Another conflicting text fromserver
 	//Local: Another conflicting text from local
+	//Local: pre-last line
 	//Local2: added text
 	//Server: Last line
 
