560476c81f1a25a416434deac4db40fe01a37fe0
abramj <abramjujunashvili@yahoo.com>
2013-06-29 17:18:13 -0600

    added only to local

--- src/test/Main.java
+++ src/test/Main.java
@@ -2,9 +2,11 @@ package test;
 
 public class Main {
 	
+	//Local3: added again
 	//comment
 	//Local: this is the conflicting text
 	//Another conflicting text fromserver
 	//Local: Another conflicting text from local
+	//Local2: added text
 
 }
