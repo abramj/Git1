7c15aa71b1b2610e1002f6e36b1263dc2fd11a44
abramj <abramjujunashvili@yahoo.com>
2013-07-07 11:53:49 -0600

    Create CreatedOnGit3.java

---
+++ src/test/CreatedOnGit3.java
@@ -0,0 +1,20 @@
+  public class CreatedOnGit3.java{
+  
+  //Server: Line 1
+	//Local3: added again
+	//Local: second line
+	//comment
+	//Local: this is the conflicting text
+	//Another conflicting text from server
+	//Local: Another conflicting text from local
+	//Local: pre-last line
+	//Local2: added text
+	//Server: Last line
+	//Local2: Last Line
+	
+	//Local: More lines
+	//Local2: More lines
+	//Server2: More Lines Local3: conflict
+	//Server 10: This is a conflict Local 10: This is a local conflicting line
+
+}
