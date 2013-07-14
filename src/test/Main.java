53b2d8b862522163fd7c6ef6dcdbf1c88a1a1322
abramj <abramjujunashvili@yahoo.com>
2013-07-14 07:42:02 -0600

    c1

--- src/test/Main.java
+++ src/test/Main.java
@@ -19,5 +19,6 @@ public class Main {
 	//Server2: More Lines Local3: conflict
 	//Server 10: This is a conflict Local 10: This is a local conflicting line
 	String s7141 = "String Server 071413-1";
+	String local7141 = "String Local 071413-1";
 
 }
