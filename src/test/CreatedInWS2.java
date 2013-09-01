667b24778c64a2c4b42255fea8c5e9f321851e3e
abramj <abramjujunashvili@yahoo.com>
2013-09-01 11:42:53 -0600

    Comm 1

--- src/test/CreatedInWS2.java
+++ src/test/CreatedInWS2.java
@@ -8,5 +8,29 @@ public class CreatedInWS2 {
 	String l2 = "L 2";
 	String local0901_1 = "Local 0901_1";
 	String local0901_2 = "Local 0901_2";
+	String L0901_7 = "String Local 090113-7"; // do not commit
+
+	private String getNewString() {
+		String ts3 = concatStrs();
+		
+		return ts3;
+
+	}
+
+	
+	private String getNewString1() {
+		String ts3 = concatStrs();
+		
+		return ts3;
+
+	}
+	
+
+	private String concatStrs() {
+		String ts = "abc";
+		String ts2 = ts;
+		String ts3 = ts + ts2;
+		return ts3;
+	}
 
 }
