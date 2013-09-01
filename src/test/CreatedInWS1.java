ad4ffbd95996d7b6d232d858e551a570070a09b2
abramj <abramjujunashvili@yahoo.com>
2013-09-01 09:36:25 -0600

    Update CreatedInWS1.java

--- src/test/CreatedInWS1.java
+++ src/test/CreatedInWS1.java
@@ -10,13 +10,15 @@ public class CreatedInWS1 {
 	  String local4 = "Local 4";
 	  String local5 = "Local 5";
 	  String L0901_4 = "String Local 090113-4";
-	  String L7141 = "String Local 071413-1";	  
+	  String L7141 = "String Local 071413-1";
+	  String S0901_6 = "Added on server 0901-6";
 	  String L7142 = "String Local 071413-2";
 	  String L7145 = "String Local 071413-5";
 	  String L7147 = "String Local 071413-7";
 	  String L7148 = "String Local 071413-8";
 	  String L0901 = "String Local 090113-1";
 	  String S0901 = "Added on server 0901-1";
+	  String S0901_6_2 = "Added on server 0901-6_2";
 	  String S0901_2 = "Added on server 0901-2";
 	  String L0901_2 = "String Local 090113-2";
 	  String S0901_3 = "Added on server 0901-3";
@@ -25,6 +27,7 @@ public class CreatedInWS1 {
 	  String L0901_4_2 = "String Local 090113-4_2";
 	  String S0901_5 = "Added on server 0901-5";
 	  String L0901_5 = "String Local 090113-5";
+	  String S0901_6_3 = "Added on server 0901-6_3";
 
 
 }
